baaccd40e4da2dc6c34f0081d97be0de516dc75a
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2024-10-15 03:46:36 +0500

    Quotes App NextJS

--- src/app/components/QuoteCard.jsx
+++ src/app/components/QuoteCard.jsx
@@ -18,7 +18,7 @@ export default function QuoteCard({author, quote, key}){
           <h1 className="cardFooter flex items-center justify-between font-bold mt-1">
             <span>Quote By: <span className="text-emerald-400">{author}</span></span>
              <span>
-               <button onClick={copyFunction} className="btn bg-emerald-200 text-black w-20 hover:bg-emerald-300 p-1 rounded-md font-bold">{copy? "Copied!" : "Copy"}</button>
+               <button title="Copy to clipboard" onClick={copyFunction} className="btn bg-emerald-200 text-black hover:bg-emerald-700 hover:text-white w-20 p-1 rounded-md font-bold">{copy? "Copied!" : "Copy"}</button>
              </span>
           </h1>
         </div>

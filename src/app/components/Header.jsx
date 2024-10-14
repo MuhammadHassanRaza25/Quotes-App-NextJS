69d6419729da3e57788a768c3faa3093f52e6beb
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2024-10-14 22:40:26 +0500

    Quotes App NextJS

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,8 +5,9 @@ export default function Header(){
         <div className="nav flex justify-between items-center px-10 py-4 bg-emerald-200">
             <div className="font-bold text-3xl navHeading">Quotes App</div>
 
-            <div className="flex justify-between gap-5">
+            <div className="flex justify-between gap-5">  
                 <Link href={'https://github.com/MuhammadHassanRaza25'} target="_blank">
+                  {/* <img width={'40px'} src={'https://cdn-icons-png.flaticon.com/512/25/25231.png'}/>    */}
                   <button className="btn navBtn hover:bg-white hover:text-black bg-black text-white w-28 p-1 rounded-md font-bold text-xl">Github</button>
                 </Link>
                 <Link href={'https://www.linkedin.com/in/muhammad-hassan-raza-aab5402b7/'} target="_blank">
--- src/app/components/QuoteCard.jsx
+++ src/app/components/QuoteCard.jsx
@@ -13,11 +13,13 @@ export default function QuoteCard({author, quote, key}){
 
     
     return(
-        <div className="quoteCard h-32 flex flex-col justify-between border border-gray-200 rounded-xl mx-14 mt-7 p-4">
-          <h1 className="scrollbar font-semibold h-22 text-xl overflow-scroll">{quote}</h1>
-          <h1 className="flex justify-between items-center font-bold mt-1">
+        <div className="quoteCard w-[70%] mx-auto flex flex-col justify-between border border-gray-200 rounded-xl mt-7 p-4">
+          <h1 className="font-semibold text-xl">{quote}</h1>
+          <h1 className="cardFooter flex items-center justify-between font-bold mt-1">
             <span>Quote By: <span className="text-emerald-400">{author}</span></span>
-            <button onClick={copyFunction} className="btn bg-emerald-200 text-black w-20 hover:bg-emerald-300 p-1 rounded-md font-bold">{copy? "Copied!" : "Copy"}</button>
+             <span>
+               <button onClick={copyFunction} className="btn bg-emerald-200 text-black w-20 hover:bg-emerald-300 p-1 rounded-md font-bold">{copy? "Copied!" : "Copy"}</button>
+             </span>
           </h1>
         </div>
     )
